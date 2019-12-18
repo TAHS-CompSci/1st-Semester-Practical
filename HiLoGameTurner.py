d8b1018a949ecab93c5657d50bbf7fc96f0d24b5
bt333 <57916904+bt333@users.noreply.github.com>
2019-12-18 13:13:21 -0600

    HiLoGame
    worked on our HiLoGame

---
+++ HiLoGameTurner.py
@@ -0,0 +1,18 @@
+"""
+projectName.py
+Programmer: Your Name
+Project description
+"""
+# Your code goes below
+
+
+
+def main():
+    """This function runs the entire program.
+        Once your function is created place it below.
+        You may need to provide extra code to run your function several times."""
+
+
+# Allows the main function to show up in the terminal once script is executed. 
+if __name__ == "__main__":
+    main()
