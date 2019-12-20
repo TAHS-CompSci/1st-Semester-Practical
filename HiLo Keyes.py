075e6983c0fc89a2bd363837f34de91f0e7c18c0
tk3078 <57916821+tk3078@users.noreply.github.com>
2019-12-20 11:51:57 -0600

    Finished the HiLo game it was a bit of a pain but I got it

--- HiLo Keyes.py
+++ HiLo Keyes.py
@@ -1,1 +1,36 @@
 import random
+
+guessesTaken = 0
+
+print("hello! what is your name?")
+myName = input()
+
+number = random.randint (1,100)
+print("well,thats a pretty cool name, anyway I'm thinking of a number between 1and 100.")
+
+while guessesTaken < 10:
+    print("take a guess.")
+    guess = input()
+    guess = int(guess)
+
+    guessesTaken = guessesTaken + 1
+
+    if guess < number:
+        print("your guess is too low")
+
+    if guess > number:
+        print ("your guess is too high")
+
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken)
+    print("Good job, you guessed my number in under 10 guesses")
+
+
+if guess!= number:
+    number = str(number)
+    print("Sorry you ran out of guesses better luck next time.")
+
+
