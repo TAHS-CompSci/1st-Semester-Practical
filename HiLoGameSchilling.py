d5124b9ec59ed497d8b8d9bb68d7af4d7b5dc9cb
cs45044504 <cs4504@k12.sd.us>
2019-12-20 11:58:53 -0600

    day 3
    finished

--- HiLoGameSchilling.py
+++ HiLoGameSchilling.py
@@ -1,18 +1,30 @@
 """
-projectName.py
-Programmer: Your Name
-Project description
+HiLoGameSchilling
+Programmer: Cole Schilling
+HiLoGame
 """
-# Your code goes below
+import random
 
-
-
-def main():
-    """This function runs the entire program.
-        Once your function is created place it below.
-        You may need to provide extra code to run your function several times."""
-
-
-# Allows the main function to show up in the terminal once script is executed. 
-if __name__ == "__main__":
-    main()
+def HiLo():
+    target = random.randint(1,100)
+    tries = 10
+    guesses = True
+    while guesses:
+        guess = int(input("Guess any number between 1 thru 100. "))
+        if guess == target:
+            print('Correct!, do you wish to play again')
+            print('tries: ',tries,)
+        if guess < target:
+            tries-=1
+            print("Higher!")
+            print('tries: ',tries,)
+            if tries == 0:
+                print('game over.. better luck next time')
+                guesses = False
+        if guess > target:
+            tries-=1
+            print("Lower")
+            print('tries: ',tries,)
+            if tries ==0:
+                print('game over.. better luck next time')
+                guesses = False
